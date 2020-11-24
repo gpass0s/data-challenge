067bb6886378c762ec53768463b7823ab739979f
Guilherme Passos <guilhermepassos@outlook.com>
2020-11-23 23:47:48 -0300

    Integrate json_schema_to_hive module with jsonschema2athena subpackage

--- desafios/exercicio2/json_schema_to_hive.py
+++ desafios/exercicio2/json_schema_to_hive.py
@@ -1,25 +1,43 @@
+from jsonschema2athena.generator import generate_query
+
 _ATHENA_CLIENT = None
 
+
 def create_hive_table_with_athena(query):
-    '''
+    """
     Função necessária para criação da tabela HIVE na AWS
     :param query: Script SQL de Create Table (str)
     :return: None
-    '''
-    
+    """
+
     print(f"Query: {query}")
     _ATHENA_CLIENT.start_query_execution(
         QueryString=query,
-        ResultConfiguration={
-            'OutputLocation': f's3://iti-query-results/'
-        }
+        ResultConfiguration={"OutputLocation": f"s3://iti-query-results/"},
     )
 
-def handler():
-    '''
+
+def handler(
+    database=None,
+    table=None,
+    schema=None,
+    location=None,
+    serde=None,
+    serdeproperties=None,
+):
+    """
     #  Função principal
     Aqui você deve começar a implementar o seu código
     Você pode criar funções/classes à vontade
     Utilize a função create_hive_table_with_athena para te auxiliar
         na criação da tabela HIVE, não é necessário alterá-la
-    '''
+    """
+    query = generate_query(
+        database=database,
+        table=table,
+        schema=schema,
+        location=location,
+        serde=serde,
+        serdeproperties=serdeproperties,
+    )
+    create_hive_table_with_athena(query)
